19ea3cb93747a9bb63ad93660c0e775419f94743
selvin-rivera <nrivera.clements@gmail.com>
2024-08-01 10:42:13 -0600

    creando conexcion api

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://api.escuelajs.co/api/v1/users")
     .then(response => response.json())
     .then(data => {
-        let card = '<div class="row row-cols-1 row-cols-md-3 g-4">'
+        let card = `<div class="row row-cols-1 row-cols-md-3 g-4">`
         for (let user of data) {
              card += `<div class="col">
                     <div class="card">
-                        <img src="https://www.cavsi.com/espanol/wp-content/uploads/informatica/usuario-de-computadoras.jpg" class="card-img-top" alt="Usuario">
+                        <img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" class="card-img-top" alt="Usuario">
                         <div class="card-body">
                             <h5 class="card-title">${user.name}</h5>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><strong>Correo electrónico:</strong> ${user.email}</li>
-                                <li class="list-group-item"><strong>Calle:</strong> ${user.password}</li>
+                                <li class="list-group-item"><strong>Contraseña:</strong> ${user.password}</li>
+                                <li class="list-group-item"><strong>Avatar:</strong> ${user.avatar}</li>
                             </ul>
                         </div>
                     </div>
                 </div>`
         }
+            card += "</div>"
+
+    document.getElementById("list-card").innerHTML = card
 
 }) 
